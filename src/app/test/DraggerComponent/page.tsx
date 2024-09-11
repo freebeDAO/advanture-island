200a5f11f0445aa1cf600278b0644758f485203a
front_clone <17381936722@163.com>
2024-09-11 19:08:53 +0800

    feat: 添加部分测试代码

--- src/app/test/DraggerComponent/page.tsx
+++ src/app/test/DraggerComponent/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import DraggerComponent from 'src/components/DraggerComponent';
 
 function Home(props: any) {
   return (
     <div className="min-h-screen p-4 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
+      <DraggerComponent/>
     </div>
   );
 }
--- src/app/test/JumpableComponent/page.tsx
+++ src/app/test/JumpableComponent/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import JumpableComponent from 'src/components/JumpableComponent';
 
 function Home(props: any) {
   return (
     <div className="min-h-screen p-4 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
+      <JumpableComponent/>
     </div>
   );
 }
