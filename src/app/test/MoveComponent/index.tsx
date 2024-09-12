f03560eea5b5de982291e34e3cbdb57cfa7c4982
front_clone <17381936722@163.com>
2024-09-12 13:23:19 +0800

    feat: 添加MoveComponent组件-Demo

--- src/app/test/MoveComponent/index.tsx
+++ src/app/test/MoveComponent/index.tsx
@@ -1,12 +1,23 @@
 'use client';
 
-import React from 'react';
+import React, { SetStateAction } from 'react';
 import MoveComponent from 'src/components/MoveComponent';
+
+type ChangeValFn = (value: SetStateAction<{ x: number; y: number; }>) => void
+
+const autoTrackChange = (changeVal: ChangeValFn, checkRun: () => boolean) => {
+  setInterval(() => {
+    if (checkRun()) return
+    changeVal((cached) => {
+      return { ...cached, y: cached.y + 5 }
+    })
+  }, 1000);
+}
 
 function Home(props: any) {
   return (
     <div className="min-h-screen p-4 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <MoveComponent step={10} />
+      <MoveComponent step={10} elMoveChaneg={autoTrackChange}/>
     </div>
   );
 }
