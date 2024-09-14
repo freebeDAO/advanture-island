495640130670dc7189e745ff73f901b12fbb648d
wenonatrammerdhh7@gmail.com <wenonatrammerdhh7@gmail.com>
2024-09-14 16:06:55 +0800

    wechat_name: Sawano task: 让系统支持钱包登录功能

--- src/server/db/sqlFile/users.sql
+++ src/server/db/sqlFile/users.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80033 (8.0.33)
  File Encoding         : 65001
 
- Date: 14/09/2024 14:06:15
+ Date: 14/09/2024 16:06:22
 */
 
 SET NAMES utf8mb4;
@@ -25,6 +25,8 @@ CREATE TABLE `users` (
   `address` varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `create_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
+  `username` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `avatar` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
   PRIMARY KEY (`address`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
