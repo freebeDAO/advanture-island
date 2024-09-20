523187552ed263b979db3547e383f8f49700daec
liliang <zkyeu@163.com>
2024-09-21 00:46:00 +0800

    feat: StyleControlComponentDemo

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -2,16 +2,16 @@
  * @Author: liliang
  * @Date: 2024-09-20 18:43:28
  * @LastEditors: liliang
- * @LastEditTime: 2024-09-20 18:45:13
+ * @LastEditTime: 2024-09-20 19:19:09
  * @FilePath: /advanture-island/src/app/test/page.tsx
  * @Description: 
  */
  "use client"
 import React from 'react';
-import ScaleComponentDemo from '../../components/test/ScaleComponentDemo';
+import StyleControlComponentDemo from '../../components/test/StyleControlComponentDemo';
 
 const Page = () => {
-  return <ScaleComponentDemo />;
+  return <StyleControlComponentDemo />;
 };
 
 export default Page;
---
+++ src/components/component/StyleControlComponent.tsx
@@ -0,0 +1,100 @@
+/*
+ * @Author: liliang
+ * @Date: 2024-09-20 19:13:18
+ * @LastEditors: liliang
+ * @LastEditTime: 2024-09-20 21:17:27
+ * @FilePath: /advanture-island/src/components/component/StyleControlComponent.tsx
+ * @Description: 
+ */
+import React, { CSSProperties } from 'react';
+
+interface StyleControlComponentProps {
+  shape: 'circle' | 'ellipse' | 'rectangle' | 'rounded' | 'triangle';
+  backgroundColor: string;
+  borderSize: number;
+  borderColor: string;
+  text: string;
+  fontSize: number;
+  fontColor: string;
+  textPosition: 'top-left' | 'top-center' | 'top-right' | 'middle-left' | 'middle-center' | 'middle-right' | 'bottom-left' | 'bottom-center' | 'bottom-right';
+}
+
+const StyleControlComponent: React.FC<StyleControlComponentProps> = ({
+  shape,
+  backgroundColor,
+  borderSize,
+  borderColor,
+  text,
+  fontSize,
+  fontColor,
+  textPosition,
+}) => {
+  // 处理不同形状的样式
+  const getShapeStyle = (): CSSProperties => {
+    switch (shape) {
+      case 'circle':
+        return { borderRadius: '50%' };
+      case 'ellipse':
+        return { borderRadius: '50%', width: '150px', height: '100px' };
+      case 'rectangle':
+        return {};
+      case 'rounded':
+        return { borderRadius: '15px' };
+      case 'triangle':
+        return {
+          width: 0,
+          height: 0,
+          borderLeft: '50px solid transparent',
+          borderRight: '50px solid transparent',
+          borderBottom: `100px solid ${backgroundColor}`,
+          backgroundColor: 'transparent',
+        };
+      default:
+        return {};
+    }
+  };
+
+  // 文本位置样式
+  const getTextPositionStyle = (): CSSProperties => {
+    const positionMap: { [key: string]: CSSProperties } = {
+      'topLeft': { justifyContent: 'flex-start', alignItems: 'flex-start' },
+      'topCenter': { justifyContent: 'center', alignItems: 'flex-start' },
+      'topRight': { justifyContent: 'flex-end', alignItems: 'flex-start' },
+      'middleLeft': { justifyContent: 'flex-start', alignItems: 'center' },
+      'middleCenter': { justifyContent: 'center', alignItems: 'center' },
+      'middleRight': { justifyContent: 'flex-end', alignItems: 'center' },
+      'bottomLeft': { justifyContent: 'flex-start', alignItems: 'flex-end' },
+      'bottomCenter': { justifyContent: 'center', alignItems: 'flex-end' },
+      'bottomRight': { justifyContent: 'flex-end', alignItems: 'flex-end' },
+    };
+    return positionMap[textPosition];
+  };
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        ...getTextPositionStyle(),
+        width: '300px',
+        height: '300px',
+        backgroundColor: shape === 'triangle' ? 'transparent' : backgroundColor,
+        border: shape !== 'triangle' ? `${borderSize}px solid ${borderColor}` : 'none',
+        ...getShapeStyle(),
+        position: 'relative',
+        overflow: 'hidden',
+      }}
+    >
+      <span
+        style={{
+          fontSize: `${fontSize}px`,
+          color: fontColor,
+          textAlign: 'center',
+        }}
+      >
+        {text}
+      </span>
+    </div>
+  );
+};
+
+export default StyleControlComponent;
