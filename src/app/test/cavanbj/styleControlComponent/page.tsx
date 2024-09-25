df21a28e60ab09dc2d43534f6416ce5bd489caf7
cavanwang <cavanxf@163.com>
2024-09-25 12:36:54 +0800

    支持组件文本位置、颜色、边框、背景色设置

--- src/app/test/cavanbj/styleControlComponent/page.tsx
+++ src/app/test/cavanbj/styleControlComponent/page.tsx
@@ -12,29 +12,41 @@ export default function Home() {
         <div>
             <div style={{float: 'left', margin: '10px'}}>
                 <ShapeComponent
+                    text="左下圆形"
+                    textPosition={{xAlign: "left", yAlign: "bottom"}}
                     shapeType="circle"
-                    width="100"
+                    width="120"
                     backgroundColor="blue"
+                    fontColor='rgb(200,50,20)'
+                    fontSize='10px'
                 />
             </div>
             <div style={{float: 'left', margin: '10px'}}>
             <ShapeComponent
+                text="右顶椭圆"
+                textPosition={{xAlign: "right", yAlign: "top"}}
                 shapeType="ellipse"
                 width="150"
                 height="100"
-                backgroundColor="green"
+                backgroundColor="yellow"
                 />
             </div>
             <div style={{float: 'left', margin: '10px'}}>
             <ShapeComponent
+                text="左中矩形"
                 shapeType="rectangle"
                 width="120"
                 height="80"
+                fontSize='10px'
+                fontColor='black'
+                textPosition={{xAlign: "left", yAlign: "middle"}}
                 backgroundColor="orange"
             />
             </div>
             <div style={{float: 'left', margin: '10px'}}>
             <ShapeComponent
+                text="中下圆矩形"
+                textPosition={{xAlign: "center", yAlign: "bottom"}}
                 shapeType="roundedRectangle"
                 width="120"
                 height="80"
@@ -43,11 +55,53 @@ export default function Home() {
             </div>
             <div style={{float: 'left', margin: '10px'}}>
             <ShapeComponent
+                text="右下三角形"
                 shapeType="triangle"
-                width="100"
-                backgroundColor="purple"
-            />
+                width="80"
+                textPosition={{xAlign: "right", yAlign: "bottom"}}
+                backgroundColor="SteelBlue"
+                borderColor='Gold'
+                fontColor='darkViolet'
+                fontSize='10px'
+                borderWidth='4px'/>
             </div>
+            <div style={{float: 'left', margin: '10px'}}>
+            <ShapeComponent
+                text="中中三角形"
+                shapeType="triangle"
+                width="120"
+                textPosition={{xAlign: "center", yAlign: "middle"}}
+                backgroundColor="black"
+                borderColor='red'
+                fontColor='darkViolet'
+                fontSize='14px'
+                borderWidth='2px'/>
+            </div>
+            <div style={{float: 'left', margin: '10px'}}>
+            <ShapeComponent
+                text="左中三角形"
+                shapeType="triangle"
+                width="120"
+                textPosition={{xAlign: "left", yAlign: "middle"}}
+                backgroundColor="black"
+                borderColor='red'
+                fontColor='darkViolet'
+                fontSize='14px'
+                borderWidth='2px'/>
+            </div>
+            <div style={{float: 'left', margin: '10px'}}>
+            <ShapeComponent
+                text="中顶三角形"
+                shapeType="triangle"
+                width="120"
+                textPosition={{xAlign: "center", yAlign: "top"}}
+                backgroundColor="black"
+                borderColor='yellow'
+                fontColor='darkViolet'
+                fontSize='14px'
+                borderWidth='2px'/>
+            </div>
+            
         </div>
         <h2 className="text-2xl h-80 text-gray-400 border border-gray-300 p-2 w-full">
           添加任务组件用于展示
--- src/components/cavanbj/styleControlComponent/styleControlComponent.tsx
+++ src/components/cavanbj/styleControlComponent/styleControlComponent.tsx
@@ -2,16 +2,84 @@
 
 import React from 'react';
 
+
+
 interface ShapeProps {
   shapeType: 'circle' | 'ellipse' | 'rectangle' | 'triangle' | 'roundedRectangle';
-  width: string;
-  height?: string;
-  backgroundColor: string;
+  width: number;
+  height?: number;
+  backgroundColor?: string;
+  text?: string;
+  fontSize?: string;
+  fontColor?: string;
+  textPosition?: { [key: string]: string};
+  borderColor?: string;
+  borderWidth?: string;
+  borderRadius?: string;
 }
 
 class ShapeComponent extends React.Component<ShapeProps> {
   render() {
-    const { shapeType, width, height, backgroundColor } = this.props;
+    let {shapeType, width, height, backgroundColor, text, fontSize, fontColor, textPosition, borderColor, borderWidth,borderRadius } = this.props;
+      // 定义字体位置的 CSS 类
+    let positionClass: {[key:string]: string} = {
+      top: 'flex-start',
+      middle: 'center',
+      bottom: 'flex-end',
+      left: 'flex-start',
+      center: 'center',
+      right: 'flex-end',
+    };
+    shapeType = shapeType || 'circle';
+    // 根据 position props 生成 CSS 类名
+    let defaultPosition = {yAlign: 'middle', xAlign: 'center'};
+    let xPositionClass = positionClass[(textPosition|| defaultPosition).xAlign || 'center'];
+    let yPositionClass = positionClass[(textPosition|| defaultPosition).yAlign || 'center'];
+
+    width = width || 50;
+    let {x, y} = [0, 0];
+    if (shapeType == 'triangle') {
+      positionClass = {
+        top: 'text-before-edge',
+        middle: 'central',
+        bottom: 'text-after-edge',
+        left: 'start',
+        center: 'middle',
+        right: 'end',
+      };
+       // 根据 position props 生成 CSS 类名
+      defaultPosition = {yAlign: 'middle', xAlign: 'center'};
+      xPositionClass = positionClass[(textPosition|| defaultPosition).xAlign || 'center'];
+      yPositionClass = positionClass[(textPosition|| defaultPosition).yAlign || 'center'];
+
+      // svg需要根据居中位置，设置文本对齐的基准点(x,y)相对父容器的位置，然后positionClass是相对于x,y这个基准点，指明字体positionClass位置要对齐到(x,y)点
+      // 水平对齐需要设置x
+      if (xPositionClass == positionClass.center) {
+        x = width/2;
+      } else if (xPositionClass == positionClass.left) {
+        x = 0;
+      } else {
+        x = width;
+      }
+      // 垂直对齐需要设置y
+      if (yPositionClass == positionClass.top) {
+        y = 0;
+      } else if (yPositionClass == positionClass.middle) {
+        y = width/2;
+      } else {
+        y = width;
+      }
+    }
+
+    // 定义默认值
+    height = height || 50;
+    backgroundColor = backgroundColor || 'white';
+    text = text || '';
+    fontSize = fontSize || '12px';
+    fontColor = fontColor || 'rgb(100,200,50)';
+    borderColor = borderColor || 'black';
+    borderWidth = borderWidth || '1px';
+    borderRadius = borderRadius || '3px';
 
     let shape;
     switch (shapeType) {
@@ -23,8 +91,15 @@ class ShapeComponent extends React.Component<ShapeProps> {
               height: width+'px',
               borderRadius: '50%',
               backgroundColor: backgroundColor,
-            }}
-          />
+              alignItems: yPositionClass,
+              justifyContent: xPositionClass,
+              fontSize: fontSize,
+              color: fontColor,
+              borderColor: borderColor,
+              borderWidth: borderWidth,
+              display: 'flex',
+              borderStyle: 'solid',
+            }}>{text}</div>
         );
         break;
       case 'ellipse':
@@ -35,8 +110,15 @@ class ShapeComponent extends React.Component<ShapeProps> {
               height: height+'px',
               borderRadius: '50%',
               backgroundColor: backgroundColor,
-            }}
-          />
+              alignItems: yPositionClass,
+              justifyContent: xPositionClass,
+              fontSize: fontSize,
+              color: fontColor,
+              borderColor: borderColor,
+              borderWidth: borderWidth,
+              display: 'flex',
+              borderStyle: 'solid',
+            }}>{text}</div>
         );
         break;
       case 'rectangle':
@@ -46,21 +128,25 @@ class ShapeComponent extends React.Component<ShapeProps> {
               width: width+'px',
               height: height+'px',
               backgroundColor: backgroundColor,
-            }}
-          />
+              alignItems: yPositionClass,
+              justifyContent: xPositionClass,
+              fontSize: fontSize,
+              color: fontColor,
+              borderColor: borderColor,
+              borderWidth: borderWidth,
+              display: 'flex',
+              borderStyle: 'solid',
+            }}>{text}</div>
         );
         break;
       case 'triangle':
         shape = (
-          <div
-            style={{
-              width: 0,
-              height: 0,
-              borderLeft: `${Number(width)/2}px solid transparent`,
-              borderRight: `${Number(width)/2}px solid transparent`,
-              borderBottom: `${Math.sqrt(3)*Number(width)/2}px solid skyblue`,
-            }}
-          />
+          <div>
+            <svg style={{width: width, height: width, display: 'flex', alignItems: 'center', justifyContent: 'flex-end'}} xmlns="http://www.w3.org/2000/svg">
+  <polygon points={`0, ${width * 0.87}, ${width * 0.5}, 0, ${width}, ${width * 0.87}`} fill={backgroundColor} stroke={borderColor} strokeWidth={borderWidth}/>
+  <text x={x} y={y} text-anchor={positionClass[textPosition.xAlign]} dominant-baseline={positionClass[textPosition.yAlign]} fill={fontColor} style={{fontSize: fontSize}}>{text}</text>
+</svg>
+          </div>
         );
         break;
       case 'roundedRectangle':
@@ -70,9 +156,16 @@ class ShapeComponent extends React.Component<ShapeProps> {
               width: width+'px',
               height: height+'px',
               backgroundColor: backgroundColor,
-              borderRadius: '10px', // 可以根据需要调整圆角大小
-            }}
-          />
+              borderRadius: borderRadius,
+              alignItems: yPositionClass,
+              justifyContent: xPositionClass,
+              fontSize: fontSize,
+              color: fontColor,
+              borderColor: borderColor,
+              borderWidth: borderWidth,
+              display: 'flex',
+              borderStyle: 'solid',
+            }}>{text}</div>
         );
         break;
       default:
